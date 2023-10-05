0c05749fca2f8ed8e92553bae5641afd01757fda
anxoaragundi <anxoaragundimesias21@gmail.com>
2023-10-05 18:52:54 +0200

    sessio5

---
+++ dia5/convertirBase.js
@@ -0,0 +1,14 @@
+function convertir() {
+
+    let num = parseInt(prompt("Introdueix un número"));
+
+    let n2 = binari(num);
+    let n8 = octal(num);
+    let n16 = hexagesimal(num);
+
+}
+
+function binari(num) {
+
+}
+
