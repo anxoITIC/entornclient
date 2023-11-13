8f9448eb21bff7310deb00751556d4396b7b01e6
anxoaragundi <anxoaragundimesias21@gmail.com>
2023-11-13 19:56:53 +0100

    versio consola casi completa

--- dia12/PenjatA/penjat.js
+++ dia12/PenjatA/penjat.js
@@ -1,35 +1,100 @@
 function Penjat() {
+    let partidesGuanyades = 0;
+    let partidesJugades = 0;
+    let partidesPerdudes = 0;
+
+    SeleccioMode(partidesGuanyades, partidesJugades, partidesPerdudes);
+}
+
+function SeleccioMode(partidesGuanyades, partidesJugades, partidesPerdudes) {
     console.log("1. Iniciar una partida");
     console.log("2. Estadístiques");
     console.log("3. Sortir del joc");
 
     let seleccio =  prompt("Seleccioni una opció");
 
+    //selecció de mode
     if (seleccio == 1) {
-        Partida();
+        partida(partidesGuanyades);
+        partidesJugades++;
     } else if (seleccio == 2) {
-        Estadistiques();
+        Estadistiques(partidesGuanyades, partidesJugades, partidesPerdudes);
     } else if (seleccio == 3) {
         Sortir();
     }
 }
 
-function Partida() {
+function partida(partidesGuanyades) {
+    var wordToGuess = prompt("Introdueix una paraula:").toLowerCase(); //demana una paraula i la processa
 
-    let paraula = prompt("Introdueix una paraula");
+    
+    if (!/^[a-z]+$/.test(wordToGuess)) {    //comprovació de si la paraula és vàlida
+        console.log("Paraula invàlida. Siusplau, introdueix una altra paraula");
+        partida(); 
+        return;
+    }
 
-    let lletresRestants = paraula.length;
-    const 
+    var guessedWord = "_".repeat(wordToGuess.length);   //mostrar la paraula convertida a guions baixos
 
-    let guions = "_";
-    while (lletresRestants > 0) {
-        console.log(guions.repeat(lletresRestants));
-        lletresRestants = lletresRestants - 1;
+    console.log("Endevina la paraula: " + guessedWord);
+
+    playGame(wordToGuess, guessedWord, partidesGuanyades);
+
+}
+
+function playGame(wordToGuess, guessedWord, partidesGuanyades) {
+    var maxAttempts = 6; 
+    var incorrectAttempts = 0;
+    var lletresIncorrectes = "";
+    
+    while (guessedWord !== wordToGuess && incorrectAttempts < maxAttempts) {
+        var guess = prompt("Introdueix una lletra:").toLowerCase();
+        
+        if (guess.length !== 1 || !/[a-z]/.test(guess)) {
+            console.log("Paraula invàlida. Siusplau escriu una diferent");
+            continue;
+        }
+        
+        if (wordToGuess.includes(guess)) {
+            console.log("Correcte!");
+            console.log("Lletres incorrectes: " + lletresIncorrectes);
+            
+            
+            for (var i = 0; i < wordToGuess.length; i++) {
+                if (wordToGuess[i] === guess) {
+                    guessedWord = guessedWord.substring(0, i) + guess + guessedWord.substring(i + 1);
+                }
+            }
+        } else {
+            console.log("Lletra incorrecta, torna-ho a intentar.");
+            incorrectAttempts++;
+            lletresIncorrectes = lletresIncorrectes + guess + ", ";
+            console.log("Lletres incorrectes: " + lletresIncorrectes);
+        }
+        
+        console.log(guessedWord);
+        console.log(incorrectAttempts + "/6 errors");
+    }
+
+    // Comprovació dels resultats
+    if (guessedWord === wordToGuess) {
+        console.log("Enhorabona! Has endevinat la paraula: " + wordToGuess);
+        partidesGuanyades++;
+        SeleccioMode(partidesGuanyades);
+    } else {
+        console.log("GAME OVER");
+        SeleccioMode(partidesGuanyades);
     }
 }
 
 
-function Estadistiques() {
+function Estadistiques(partidesGuanyades, partidesJugades, partidesPerdudes) {
+    console.log("ESTADÍSTIQUES");
+
+    console.log("Total de partides: " + partidesJugades);
+    console.log("Partides guanyades: " + partidesGuanyades);
+    console.log("Partides perdudes: " + partidesPerdudes);
+
 
 }
 
